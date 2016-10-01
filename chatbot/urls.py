f55e1ae3fcb209211480551745ab7bcde861ebe5
Nivedhita Sathyamurthy <sathyamn@uci.edu>
2016-10-01 10:29:58 -0700

    Adding first view

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # url(r'^$', 'chatbot.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^sathya/', include('sathya.urls')),
     url(r'^admin/', include(admin.site.urls)),
     ]
 
