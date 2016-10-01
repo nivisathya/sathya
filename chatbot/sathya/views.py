40cca68720fdee375c3ca825f9218143dbba867c
jvO1 <jvspectre@gmail.com>
2016-10-01 16:42:39 -0700

    dummy ask action

--- chatbot/sathya/views.py
+++ chatbot/sathya/views.py
@@ -4,3 +4,7 @@ import os
 def index(request):
     file = open(os.getcwd()+'/sathya/chatPage.html','r')
     return HttpResponse(file.read())
+
+def ask(request):
+    question = request.GET.get('q', '')
+    return HttpResponse(question)
