e8f9012a07c6683dc65a3435d13884a7b88a75e2
jvO1 <jvspectre@gmail.com>
2016-10-02 08:28:24 -0700

    url rule for static

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,3 +15,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     ]
 
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
