c80537ee290a9a79f1b1f6c6d46614a2e833499e
jvO1 <jvspectre@gmail.com>
2016-12-18 00:26:19 -0800

    updated wsgi

--- chatbot/chatbot/wsgi.py
+++ chatbot/chatbot/wsgi.py
@@ -8,6 +8,6 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
